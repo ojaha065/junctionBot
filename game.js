863361dc7d6288e0e850790b41dfeba13b9213db
Jani Haiko <ojaha065@edu.xamk.fi>
2019-03-31 16:42:01 +0300

    Initial Commit

---
+++ game.js
@@ -0,0 +1,151 @@
+"use strict";
+
+const fs = require("fs");
+const request = require("request");
+const Translate = require("@google-cloud/translate");
+
+const catAPIUrl = "https://api.thecatapi.com/v1/images/search";
+const catFactsAPIUrl = "https://catfact.ninja/fact";
+
+// Fast and dirty way of keeping my Heroku instance from falling asleep
+setInterval(() => {
+    request.get({
+        url: "https://telegram-bot-gts.herokuapp.com/"
+    },(error) => {
+        if(!error){
+            console.info("Just keeping myself alive!");
+        }
+        else{
+            console.error(error);
+        }
+    });
+},600000);
+
+// Loading the JSON data
+let songs;
+let languages;
+fs.readFile("./data.json","UTF-8",(error,data) => {
+    if(!error){
+        try{
+            data = JSON.parse(data);
+        }
+        catch(error){
+            throw error;
+        }
+
+        songs = data[0];
+        languages = data[1];
+        console.info(`All songs loaded! There are ${songs.length} in total`);
+    }
+    else{
+        throw error;
+    }
+});
+
+const translator = new Translate.Translate({
+    projectId: "gts-236118"
+});
+
+module.exports = {
+    // There are much better ways of doing what I'm doing than this pyramid of doom...
+    // Also, there might be issues if many peole are playing at the same time. I didn't have the time to test that.
+    // But at least it works for a one player, okay. :D
+    round: async (ctx) => {
+        if(ctx.session.gameData.playedSongs.length >= songs.length){
+            // All songs played
+            ctx.reply(`This prototype only includes ${songs.length} songs and you played them all. Come back later for more or use /newgame to play same songs again. Thank you for playing!`);
+            ctx.session.gameData.state = 4; // Game ended
+            setTimeout(() => {
+                // Because *of course* we need some cat pictures...
+                // And finally got a good use for that Cat Facts API too... :D
+                ctx.reply("Also... Have a cat!");
+                request.get({
+                    url: catAPIUrl,
+                    json: true
+                },(error,response,body) => {
+                    if(!error && response.statusCode === 200){
+                        ctx.replyWithPhoto(body[0].url);
+                    }
+                    else{
+                        console.error(error);
+                        console.info(`HTTP response code: ${response.statusCode}`);
+                        ctx.reply("Oh no! It seems that Cat API is down! :O");
+                    }
+                    setTimeout(() => {
+                        request.get({
+                            url: catFactsAPIUrl,
+                            json: true
+                        },(error,response,body) => {
+                            if(!error && response.statusCode === 200){
+                                ctx.reply(`Did you know: ${body.fact || "There are over 500 million domestic cats in the world."}`);
+                            }
+                            else{
+                                ctx.reply("Oh no! It seems that Cat Facts API is down! :O");
+                            }
+                        });
+                    },3000);
+                })
+            },4000);
+            return;
+        }
+
+        ctx.reply(`Round ${++ctx.session.gameData.round} / ${songs.length}`);
+        if(ctx.session.gameData.round % 4 === 0){
+            setTimeout(() => {
+                ctx.reply("Tip: Use /stop to stop the current game.");
+            },1500);
+        }
+        ctx.reply("Please wait!");
+
+        while(true){
+            ctx.session.gameData.selected_index = Math.floor(Math.random() * songs.length); // Let's draw a random song
+            // Then we draw a another one until we get one that's not already been played
+            if(!ctx.session.gameData.playedSongs.includes(ctx.session.gameData.selected_index)){
+                break;
+            }
+        }
+        ctx.session.gameData.playedSongs.push(ctx.session.gameData.selected_index);
+
+        // Google Translate
+        // First, let's roll a dice and select ten languages in a random order
+        let translateTo = [];
+        for(let i = 0;i < 10;i++){
+            translateTo.push(languages[Math.floor(Math.random() * languages.length)]);
+        }
+        //console.log(translateTo);
+
+        // Then we actually call the API and do the translation, one by one
+        let translation = await translator.translate(songs[ctx.session.gameData.selected_index].lyrics,translateTo[0]);
+        translation = await translator.translate(translation[0],translateTo[1]);
+        translation = await translator.translate(translation[0],translateTo[2]);
+        translation = await translator.translate(translation[0],translateTo[3]);
+        if(!ctx.session.gameData.easyMode){
+            translation = await translator.translate(translation[0],translateTo[4]);
+            translation = await translator.translate(translation[0],translateTo[5]);
+            translation = await translator.translate(translation[0],translateTo[6]);
+            translation = await translator.translate(translation[0],translateTo[7]);
+            translation = await translator.translate(translation[0],translateTo[8]);
+            translation = await translator.translate(translation[0],translateTo[9]);
+        }
+        // Last step is ofc to translate it back to English
+        translation = await translator.translate(translation[0],(ctx.session.gameData.fi) ? "fi" : "en");
+
+        ctx.reply(`🎶\n${translation[0].replace(/\./g,"\n")}🎶\n\nWhat popular song is this according to Google Translator?`);
+        ctx.session.gameData.state = 2; // Playing, waiting for input
+    },
+    guess: (ctx,callback) => {
+        //console.log(ctx.session.gameData.answers);
+        let theGuess = ctx.message.text.toLowerCase();
+        //console.log(theGuess);
+        if(songs[ctx.session.gameData.selected_index].answers.includes(theGuess)){
+            return callback(true); // The answer was correct
+        }
+        else{
+            return callback(false);
+        }
+    },
+    skip: (ctx,callback) => {
+        ctx.reply(`The correct answer would have been ${songs[ctx.session.gameData.selected_index].answers[0].toUpperCase()} or ${songs[ctx.session.gameData.selected_index].answers[1].toUpperCase()}`);
+        return callback();
+    }
+};
---
+++ server.js
@@ -0,0 +1,156 @@
+"use strict";
+
+// Jani Haiko, 2019
+// Junction Online Challenge entry
+// March 2019
+// https://github.com/ojaha065/junctionBot
+
+// https://telegraf.js.org/
+const Telegraf = require("telegraf");
+const session = require("telegraf/session");
+
+const start = "Welcolme! Nice to have you here.\n\nUse /newgame to start the game or /help to read to instructions.";
+const help = "Instructions:\n\nUse /newgame to start. You will be presented with the Google Translators interpretation of some popular song. Your job is to guess what song it is. Easy, right? ;) If you can't remember the name of the song, you can also type the artist/band name. Use /skip to move to the next song.\n\nThis bot was made in 23 hours during the Junction Online Challenge.";
+
+// Let's create a new telegram client with Telegraf
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
+// Heroku support
+const port = process.env.PORT || 8000;
+bot.telegram.setWebhook(`https://telegram-bot-gts.herokuapp.com/bot${process.env.TELEGRAM_TOKEN}`);
+bot.startWebhook(`/bot${process.env.TELEGRAM_TOKEN}`,null,port);
+
+// To help keep server.js clean let's put most of the "boring" code inside a different file
+const game = require("./game.js");
+
+// Session control
+bot.use(session());
+bot.use((ctx,next) => {
+    // The gameData object needs to always exist
+    if(!ctx.session.gameData){
+        ctx.session.gameData = {
+            state: 0, // Not playing
+            round: 0,
+            selected_index: null, // Whitch song from the database is this user currently guessing
+            easyMode: false,
+            playedSongs: [], // This user has already guessed these songs, so they shouldn't be played again
+        };
+    }
+    return next();
+});
+
+// Common Telegram bot commands
+// Most bots support these
+bot.start((ctx) => {
+    ctx.reply(start);
+});
+bot.help((ctx) => {
+    ctx.reply(help);
+});
+bot.settings((ctx) => {
+    ctx.reply("Sorry, I don't have any settings.");
+});
+
+// Custom commands (for debugging)
+bot.command("ping",(ctx) => {
+    ctx.reply("PONG!");
+});
+bot.command("whoami",(ctx) => {
+    ctx.reply(ctx.from.id);
+});
+bot.command("clear",(ctx) => {
+    ctx.session.gameData = null;
+    ctx.reply("All data cleared!");
+});
+
+// Game logic
+bot.command(["newgame","new"],(ctx) => {
+    if(ctx.session.gameData.state !== 0){ // We reset all game data if this user has a active game
+        ctx.reply("Aborting current game...");
+        ctx.session.gameData = {
+            state: 0,
+            round: 0,
+            selected_index: null,
+            easyMode: false,
+            playedSongs: []
+        };
+    }
+
+    ctx.reply("Okay! Let's start!");
+    ctx.session.gameData.state = 1; // Starting up
+
+    setTimeout(() => {
+        game.round(ctx);
+    },1000);
+});
+bot.command(["easymode","easy"],(ctx) => {
+    if(ctx.session.gameData.state === 2){
+        // Toggling the easy mode
+        if(!ctx.session.gameData.easyMode){
+            ctx.session.gameData.easyMode = true;
+            ctx.reply("Easy mode activated!");
+        }
+        else{
+            ctx.session.gameData.easyMode = false;
+            ctx.reply("Easy mode deactivated!");
+        }
+    }
+    else{
+        ctx.reply("This command is not available right now.");
+    }
+});
+bot.command("skip",(ctx) => {
+    if(ctx.session.gameData.state === 2){
+        ctx.session.gameData.state = 3;
+        game.skip(ctx,() => {
+            setTimeout(() => {
+                game.round(ctx);
+            },1500);
+        });
+    }
+    else{
+        ctx.reply("This command is not available right now.");
+    }
+});
+bot.command("stop",(ctx) => {
+    ctx.reply("Stopping! Thank you for playing!");
+    ctx.session.gameData = null;
+});
+// Hidden command. When enabled, the last translation language is Finnish instead of English.
+// Makes the game at least 2x harder
+bot.command("suomi",(ctx) => {
+    ctx.session.gameData.fi = true;
+    ctx.reply("Suomenkielinen tila otettu käyttöön.");
+});
+bot.on("text",(ctx) => {
+    if(ctx.session.gameData.state !== 2){
+        ctx.reply("Use /newgame to start a new game or /help to get list of available commands.");
+    }
+    else{
+        ctx.session.gameData.state = 3; // Playing, waiting for the server
+        game.guess(ctx,(correct) => {
+            if(correct){
+                ctx.reply("Correct!");
+                // The answer was correct, let's wait a bit before continuing
+                setTimeout(() => {
+                    game.round(ctx);
+                },1500);
+            }
+            else{
+                ctx.reply("Sorry, that's not right. Maybe try again?");
+                if(!ctx.session.gameData.easyMode && Math.floor(Math.random() * 5) === 0){
+                    // 1/5 chance of showing this message
+                    ctx.reply("Too hard? Try /easymode and/or do /skip");
+                }
+                ctx.session.gameData.state = 2; // Playing, waiting for input
+            }
+        });
+    }
+});
+
+// Let's actually start the server
+bot.launch().then(() => {
+    console.info("The server was successfully started");
+}).catch((error) => {
+    console.error(error);
+});
